5e0c685646e24ad14044bb7c38425ef07032466e
stream-cipher-analyzer <dev@localhost>
2026-08-20 12:38:22 +0800

    Register A5/1 (fixed), remove stray PDF

--- ciphers/__init__.py
+++ ciphers/__init__.py
@@ -5,6 +5,7 @@ pass their test-vector self-check are registered here; ``a51`` / ``grain128`` /
 ``zuc256`` are added once their implementations are verified.
 """
 
+from .a51 import A51
 from .chacha20 import ChaCha20
 from .salsa20 import Salsa20
 from .trivium import Trivium
@@ -13,6 +14,7 @@ BUILTIN = {
     "chacha20": ChaCha20,
     "trivium": Trivium,
     "salsa20": Salsa20,
+    "a51": A51,
 }
 
-__all__ = ["BUILTIN", "ChaCha20", "Trivium", "Salsa20"]
+__all__ = ["BUILTIN", "ChaCha20", "Trivium", "Salsa20", "A51"]
